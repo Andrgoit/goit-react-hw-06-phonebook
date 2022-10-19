8b5e472f78102e5e1d15f24dce7eac1ccd045855
Andrgoit <stakgrom@gmail.com>
2022-10-20 02:16:29 +0300

    update bg

--- src/components/PhoneBook/PhoneBook.styled.jsx
+++ src/components/PhoneBook/PhoneBook.styled.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const StyledMain = styled.main`
   /* background-color: ${p => p.theme.colors.bgMain}; */
-  background: url('https://cdn.pixabay.com/photo/2017/07/14/10/50/wooden-planks-2503422_960_720.jpg');
+  background: url('https://cdn.pixabay.com/photo/2012/03/03/23/06/wall-21534_960_720.jpg');
   /* background-repeat: no-repeat; */
   /* background-size: cover; */
   width: 100vw;
@@ -13,12 +13,13 @@ export const StyledMain = styled.main`
 `;
 
 export const StyledContainer = styled.div`
+  background: url('http://bgfons.com/upload/plastic_texture1361.jpg');
   display: flex;
   flex-direction: column;
   gap: ${p => p.theme.space[3]}px;
   padding: ${p => p.theme.space[2]}px;
 
-  background-color: ${p => p.theme.colors.bgContainer};
+  /* background-color: ${p => p.theme.colors.bgContainer}; */
   border-radius: ${p => p.theme.radii.container};
   box-shadow: 0px 4px 8px black;
 `;
